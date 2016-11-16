0fd409d5bf4bcd04d9b57d040ace3273b459b3b3
Scubber <timothycarreira@gmail.com>
2016-11-16 10:08:44 -0500

    Initial UI layout - Needs some work

--- ManVsFood/Form1.Designer.cs
+++ ManVsFood/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ManVsFood
 {
-    partial class Form1
+    partial class Form_MVF
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lb_Items = new System.Windows.Forms.ListBox();
+            this.lb_AddedItems = new System.Windows.Forms.ListBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Remove = new System.Windows.Forms.Button();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_MVF = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_Items
+            // 
+            this.lb_Items.FormattingEnabled = true;
+            this.lb_Items.ItemHeight = 24;
+            this.lb_Items.Location = new System.Drawing.Point(12, 146);
+            this.lb_Items.Name = "lb_Items";
+            this.lb_Items.Size = new System.Drawing.Size(283, 628);
+            this.lb_Items.TabIndex = 0;
+            // 
+            // lb_AddedItems
+            // 
+            this.lb_AddedItems.FormattingEnabled = true;
+            this.lb_AddedItems.ItemHeight = 24;
+            this.lb_AddedItems.Location = new System.Drawing.Point(481, 146);
+            this.lb_AddedItems.Name = "lb_AddedItems";
+            this.lb_AddedItems.Size = new System.Drawing.Size(283, 628);
+            this.lb_AddedItems.TabIndex = 1;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(334, 371);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(110, 44);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Remove
+            // 
+            this.btn_Remove.Location = new System.Drawing.Point(334, 439);
+            this.btn_Remove.Name = "btn_Remove";
+            this.btn_Remove.Size = new System.Drawing.Size(110, 44);
+            this.btn_Remove.TabIndex = 3;
+            this.btn_Remove.Text = "Remove";
+            this.btn_Remove.UseVisualStyleBackColor = true;
+            this.btn_Remove.Click += new System.EventHandler(this.btn_Remove_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(334, 510);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(110, 44);
+            this.btn_Reset.TabIndex = 4;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(1360, 802);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(110, 44);
+            this.btn_Start.TabIndex = 5;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(1360, 852);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(110, 44);
+            this.btn_Exit.TabIndex = 6;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::ManVsFood.Properties.Resources.photo_2015_05_19_09_58_48;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(865, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(883, 812);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Challenge Time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(883, 861);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Cost of Failure:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1103, 812);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1103, 861);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // lbl_MVF
+            // 
+            this.lbl_MVF.AutoSize = true;
+            this.lbl_MVF.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MVF.Location = new System.Drawing.Point(176, 45);
+            this.lbl_MVF.Name = "lbl_MVF";
+            this.lbl_MVF.Size = new System.Drawing.Size(425, 75);
+            this.lbl_MVF.TabIndex = 12;
+            this.lbl_MVF.Text = "Man Vs Food";
+            // 
+            // Form_MVF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1576, 1016);
+            this.Controls.Add(this.lbl_MVF);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.btn_Remove);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.lb_AddedItems);
+            this.Controls.Add(this.lb_Items);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form_MVF";
+            this.Text = "Man Vs Food";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lb_Items;
+        private System.Windows.Forms.ListBox lb_AddedItems;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Remove;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_MVF;
     }
 }
 
--- ManVsFood/Form1.cs
+++ ManVsFood/Form1.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace ManVsFood
 {
-    public partial class Form1 : Form
+    public partial class Form_MVF : Form
     {
-        public Form1()
+        public Form_MVF()
         {
             InitializeComponent();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            // Adds items from lb_Items to lb_AddedItems
+        }
+
+        private void btn_Remove_Click(object sender, EventArgs e)
+        {
+            //Removes items from lb_AddedItems and Adds them to lb_Items
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            //idk my bff jill
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            //Starts a timer according to the selected item in lb_AddedItems
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            //exits the app
+            this.Close();
+            //maybe add a confirmation dialogue
+
+            //MessageBox.Show("Are you sure you want ot Exit?", "Exit", MessageBoxButtons.YesNo);
+
         }
     }
 }
